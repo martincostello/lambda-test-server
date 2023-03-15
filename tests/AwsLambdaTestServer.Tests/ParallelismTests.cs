858ad7e9ca4511d9d28ab6d03550f8b0c7b190f5
martincostello <martin@martincostello.com>
2023-03-15 14:57:43 +0000

    Fix analysis warnings
    Fix new code analysis warnings by using `CancelAsync()`.

--- tests/AwsLambdaTestServer.Tests/ParallelismTests.cs
+++ tests/AwsLambdaTestServer.Tests/ParallelismTests.cs
@@ -93,7 +93,7 @@ public static class ParallelismTests
             }
 
             completionSource.SetResult(actual);
-            cts.Cancel();
+            await cts.CancelAsync();
         });
 
         return completionSource;
