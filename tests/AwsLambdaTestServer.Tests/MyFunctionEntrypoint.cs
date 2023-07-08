286e381359004c4f5e8b5e66978af1fda090f77e
martincostello <martin@martincostello.com>
2023-07-08 07:52:07 +0100

    Remove redundant usings
    Remove redundant using statements.

--- tests/AwsLambdaTestServer.Tests/MyFunctionEntrypoint.cs
+++ tests/AwsLambdaTestServer.Tests/MyFunctionEntrypoint.cs
@@ -1,10 +1,6 @@
 // Copyright (c) Martin Costello, 2019. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace MartinCostello.Testing.AwsLambdaTestServer;
 
 internal static class MyFunctionEntrypoint
--- tests/AwsLambdaTestServer.Tests/MyHandler.cs
+++ tests/AwsLambdaTestServer.Tests/MyHandler.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Martin Costello, 2019. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Linq;
-using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 
 namespace MartinCostello.Testing.AwsLambdaTestServer;
