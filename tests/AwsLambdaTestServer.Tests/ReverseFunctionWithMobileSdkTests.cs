2a8e84f928fab326418d623171aee4fbb4b7e2c3
Martin Costello <martin@martincostello.com>
2023-07-26 14:44:34 +0100

    Update timeout
    Increase the timeout to make tests less flaky.

--- tests/AwsLambdaTestServer.Tests/ReverseFunctionWithMobileSdkTests.cs
+++ tests/AwsLambdaTestServer.Tests/ReverseFunctionWithMobileSdkTests.cs
@@ -13,7 +13,7 @@ public static class ReverseFunctionWithMobileSdkTests
     {
         // Arrange
         using var server = new LambdaTestServer();
-        using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(2));
 
         await server.StartAsync(cancellationTokenSource.Token);
 
